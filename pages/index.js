88128d2c0ba08469156cd9ce79b34688449c5555
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-16 14:29:40 +0100

    Added components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Contact from '@/components/Contact'
+import { Box } from '@chakra-ui/react'
 import About from '../components/About'
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
+import theme from '@/components/Chakra_theme'
 
 export default function Home() {
   return (
-    <About/>
+    <>
+    <Navbar/>
+    <Box pt='85px' bg={theme.colors.background}>
+      <Main/>
+      <About/>
+      <Contact/>
+    </Box>
+    </>
   )
 }
