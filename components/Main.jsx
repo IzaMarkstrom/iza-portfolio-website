03140ed0e0fa09488cfa90728153603001374864
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-16 14:29:00 +0100

    Added a Main component with avatar and text

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import React from 'react';
+import theme from './Chakra_theme';
 
-export const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+const Main = () => {
+    return (
+        <Box>
+            <Flex justifyContent='center' alignItems='center'>
+                <Avatar size='2xl' src='../assets/me2.jpg' alt='picture of me'/>
+                <Text fontSize='3xl' ml={4}>Hi, my name is <Text as='span' color={theme.colors.primary}>Iza</Text> and I'm a full stack web developer</Text>
+            </Flex>
+        </Box>
+    );
+};
+
+export default Main;
