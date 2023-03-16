a9d87ef87d48657518fb984918132d741b00ff0a
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-16 14:27:15 +0100

    Added a theme component to have my own colors

---
+++ components/Chakra_theme.jsx
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    primary: "rgb(99, 63, 211)",
+    blue: "rgb(60, 85, 169)",
+    background: "rgb(4, 0, 19)",
+    gray: "rgb(190, 190, 190)",
+    darkGray: "rgb(79, 91, 102)"
+  },
+});
+
+export default theme;
