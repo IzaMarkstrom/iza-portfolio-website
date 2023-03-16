920637a93017ff79cc0f81f949dfa5e9250e0405
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-16 14:28:38 +0100

    Added boilerplate for contact. Will be re-written later

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    FormHelperText,
+    Textarea
+  } from '@chakra-ui/react'
 
 const Contact = () => {
     return (
-        <div>
-        <h2>Contact</h2>
-        </div>
+        <>
+            <h2>Contact</h2>
+            <FormControl>
+                <FormLabel>Email address</FormLabel>
+                <Input width='550px' mt={5} placeholder='Email Address' type='email' /> <br />
+                <Textarea width='550px' mt={5} placeholder='Write your message here...' resize='none'/>
+                <FormHelperText>We'll never share your email.</FormHelperText>
+            </FormControl>
+        </>
     );
 };
 
