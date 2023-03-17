b556f7c367a9e3f8c6ddc7e5a1efb34242902528
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-17 15:05:41 +0100

    Changed Button styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,10 +20,11 @@ function Navbar() {
       top={0}
       left={0}
       right={0}
-      bg={theme.colors.primary}
       p={4}
       align="center"
-      borderBottom={showBorder ? '1px solid black' : 'none'}
+      borderBottomWidth="1px"
+      borderBottomColor="black"
+      backgroundColor="rgba(37, 59, 71, 0.9)"
       boxShadow={showBorder ? '0 8px 16px rgba(0, 0, 1, 1)' : 'none'}
       zIndex={1}
     >
@@ -32,11 +33,31 @@ function Navbar() {
       </Box>
       <Spacer />
       <Box>
-        <Button variant="ghost" color="white" mr={4}>HOME</Button>
-        <Button variant="ghost" color="white" mr={4}>ABOUT</Button>
-        <Button variant="ghost" color="white" mr={4}>PROJECTS</Button>
-        <Button variant="ghost" color="white" mr={4}>SKILLS</Button>
-        <Button variant="ghost" color="white" mr={4}>CONTACT</Button>
+        <Button
+            variant='solid' 
+            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            color='white'
+            _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>HOME</Button>
+        <Button
+            variant='solid' 
+            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            color='white'
+            _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>ABOUT</Button>
+        <Button
+            variant='solid' 
+            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            color='white'
+            _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>PROJECTS</Button>
+        <Button
+            variant='solid' 
+            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            color='white'
+            _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>SKILLS</Button>
+        <Button
+            variant='solid' 
+            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            color='white'
+            _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>CONTACT</Button>
       </Box>
     </Flex>
   );
