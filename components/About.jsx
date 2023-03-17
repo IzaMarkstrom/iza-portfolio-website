17d69c5f1a9971ba417512a31cb5c30743c3f5ab
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-17 15:04:05 +0100

    Added a Card component instead of using Box

--- components/About.jsx
+++ components/About.jsx
@@ -1,22 +1,50 @@
-import { Box, Flex, Text, Image } from '@chakra-ui/react';
+import { Box, 
+    Text, 
+    Image,
+    Stack,
+    Heading,
+    Card,
+    CardBody
+ } from '@chakra-ui/react';
 import React from 'react';
+import theme from './Chakra_theme'
 
 const About = () => {
     return (
-        <Box>
-            <Text fontSize='3xl'>About</Text>
-            <Flex>
-                <Text fontSize='xl' >I'm a full stack web developer with a passion for learning new technologies. 
-                                    I'm currently working on a project that uses React, TypeScript, NextjS, Node.js, 
-                                    Express, and MongoDB. I come from a background of many years in the travel industry. 
-                                    The pandemic let me explore my interest for programming and set me on a path of pursuing 
-                                    my dream of becomming a web developer.
-                </Text>
-                <Image 
-                    src='https://images.unsplash.com/photo-1545239351-ef35f43d514b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-                    boxSize='400px'
-                    />
-            </Flex>
+        <Box mt='200px'>
+            <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+                maxW='1500px'
+                mx='auto'
+                p={2}
+                bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                border={`1px solid ${theme.colors.darkGray}`}
+                borderRadius='10px'
+                color='white'
+                >
+
+                <Stack>
+                    <CardBody>
+                    <Heading fontSize='3xl'>About</Heading>
+
+                    <Text fontSize='2xl'>
+                        I'm a full stack web developer with a passion for learning new technologies. 
+                        I'm currently working on a project that uses React, TypeScript, NextjS, Node.js, 
+                        Express, and MongoDB. I come from a background of many years in the travel industry. 
+                        The pandemic let me explore my interest for programming and set me on a path of pursuing 
+                        my dream of becomming a web developer.
+                    </Text>
+                    </CardBody>
+                </Stack>
+                <Image
+                    objectFit='cover'
+                    maxW={{ base: '100%', sm: '400px' }}
+                    src='../assets/Presentation1.jpg'
+                    alt='Laptop and phone on a desk'
+                />
+            </Card>
         </Box>
     );
 };
