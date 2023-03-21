998ab5957deb11a74a7fe9191b249465c4c485ac
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-21 20:49:23 +0100

    Changed contact from form to button

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import {
-    FormControl,
-    FormLabel,
-    Input,
-    FormHelperText,
-    Textarea,
-    Box
+    Button,
+    Box,
+    Flex,
+    Text
   } from '@chakra-ui/react'
+import theme from './Chakra_theme';
 
 const Contact = () => {
     return (
         <Box mt='200px'>
-            <h2>Contact</h2>
-            <FormControl>
-                <FormLabel>Email address</FormLabel>
-                <Input width='550px' mt={5} placeholder='Email Address' type='email' /> <br />
-                <Textarea width='550px' mt={5} placeholder='Write your message here...' resize='none'/>
-                <FormHelperText>We'll never share your email.</FormHelperText>
-            </FormControl>
+            <Flex alignItems='center' flexDir='column'>
+                <Text fontSize='3xl'>Contact</Text>
+            </Flex>
+            <Flex justifyContent='center' flexDir='row'>
+                <Button 
+                    variant='solid' 
+                    bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`} 
+                    color='white'
+                    _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}}
+                    size='lg'
+                    p='50px'
+                    m='50px'
+                    borderRadius='20px'
+                    >Contact me!</Button>
+            </Flex>
         </Box>
     );
 };
