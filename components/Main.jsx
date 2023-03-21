3f16d74aab03bc4987ddf036027d051f4f1e90d9
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-21 20:48:20 +0100

    Added support for smaller screens like mobiles

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,35 +1,70 @@
-import { Avatar, Box, Flex, Stack, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Stack, Text, useMediaQuery } from '@chakra-ui/react';
 import React from 'react';
-import theme from './Chakra_theme';
 
 const Main = () => {
+    const [isMobile] = useMediaQuery("(max-width: 768px)");
+
     return (
-        <Box>
-            <Flex justifyContent='center' alignItems='center'>
-                <Box boxShadow="0 0 20px rgba(99, 63, 211, 0.9)" borderRadius="full" overflow="hidden">
-                    <Avatar 
-                        size='3xl' 
-                        src='../assets/me.jpg' 
-                        alt='picture of me'
-                        />
-                </Box>
-                <Stack spacing={4} ml={4}>
-                <Text
-                    fontSize='6xl'
-                    fontWeight='extrabold'
-                    >
-                    Iza Markström
-                </Text>
-                <Text
-                    fontSize='6xl'
-                    fontWeight='extrabold'
-                    >
-                    Full Stack Web Developer
-                </Text>
-                <Text as='i' fontSize='2xl'>Not everything is #000000 or #FFFFFF</Text>
-                </Stack>
-            </Flex>
-        </Box>
+        <Flex justifyContent='center'>
+            <Stack 
+                spacing={4} 
+                ml={isMobile ? 4 : 0} // set ml=0 on smaller screens
+                direction={isMobile ? "row" : "column"} // set direction to row on larger screens and column on smaller screens
+                >
+                {isMobile ? (
+                            <Flex justifyContent='center' alignItems='center'>
+                            <Stack spacing={4}>
+                                    <Avatar 
+                                        alignSelf='center'
+                                        size='xl' 
+                                        src='../assets/me.jpg' 
+                                        alt='picture of me'
+                                        />
+                                <Stack spacing={4} alignItems='center'>
+                                    <Text
+                                        fontSize='3xl'
+                                        fontWeight='extrabold'
+                                        >
+                                        Iza Markström
+                                    </Text>
+                                    <Text
+                                        fontSize='3xl'
+                                        fontWeight='extrabold'
+                                        >
+                                        Full Stack Web Developer
+                                    </Text>
+                                    <Text as='i' fontSize='xl'>Not everything is #000000 or #FFFFFF</Text>
+                                </Stack>
+                            </Stack>
+                        </Flex>
+                ) : (
+                    <Flex alignItems='center'>
+                        <Box boxShadow="0 0 20px rgba(255, 255, 255, 0.9)" borderRadius="full" overflow="hidden">
+                            <Avatar 
+                                size='3xl' 
+                                src='../assets/me.jpg' 
+                                alt='picture of me'
+                                />
+                        </Box>
+                        <Stack spacing={4} ml={4}>
+                            <Text
+                                fontSize='6xl'
+                                fontWeight='extrabold'
+                                >
+                                Iza Markström
+                            </Text>
+                            <Text
+                                fontSize='6xl'
+                                fontWeight='extrabold'
+                                >
+                                Full Stack Web Developer
+                            </Text>
+                            <Text as='i' fontSize='2xl'>Not everything is #000000 or #FFFFFF</Text>
+                        </Stack>
+                    </Flex>
+                )}
+            </Stack>
+        </Flex>
     );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Flex, Text, Button, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Text, Button, Spacer, useMediaQuery, Menu, MenuButton, MenuItem, MenuList, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import theme from './Chakra_theme';
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,33 +35,83 @@ function Navbar() {
         <Text color="white" fontWeight="bold" p={2}>My Portfolio</Text>
       </Box>
       <Spacer />
-      <Box>
-        <Button
+      {isMobile ? (
+        <Box>
+            <Menu>
+                <MenuButton
+                    as={IconButton}
+                    aria-label='Options'
+                    icon={<HamburgerIcon />}
+                    variant='outline'
+                />
+                <MenuList bg='black'>
+                    <MenuItem 
+                        bg={theme.colors.background2}
+                        _hover={{bg:theme.colors.primary, borderRadius:'30px'}} mr={4}
+                        >
+                    HOME
+                    </MenuItem>
+                    <MenuItem 
+                        bg={theme.colors.background2}
+                        _hover={{bg:theme.colors.primary, borderRadius:'30px'}} mr={4}
+                        >
+                    ABOUT
+                    </MenuItem>
+                    <MenuItem 
+                        bg={theme.colors.background2}
+                        _hover={{bg:theme.colors.primary, borderRadius:'30px'}} mr={4}
+                        >
+                    PROJECTS
+                    </MenuItem>
+                    <MenuItem 
+                        bg={theme.colors.background2}
+                        _hover={{bg:theme.colors.primary, borderRadius:'30px'}} mr={4}
+                        >
+                    SKILLS
+                    </MenuItem>
+                    <MenuItem 
+                        bg={theme.colors.background2}
+                        _hover={{bg:theme.colors.primary, borderRadius:'30px'}} mr={4}
+                        >
+                    CONTACT
+                    </MenuItem>
+            </MenuList>
+        </Menu>
+    </Box>
+      ) : (
+        <Box>
+          <Button
             variant='solid' 
             bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`} 
             color='white'
+            borderRadius='50px'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>HOME</Button>
-        <Button
+          <Button
             variant='solid' 
             bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
+            borderRadius='50px'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>ABOUT</Button>
-        <Button
+          <Button
             variant='solid' 
             bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
+            borderRadius='50px'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>PROJECTS</Button>
-        <Button
+          <Button
             variant='solid' 
             bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
+            borderRadius='50px'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>SKILLS</Button>
-        <Button
+          <Button
             variant='solid' 
             bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
+            borderRadius='50px'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>CONTACT</Button>
-      </Box>
+        </Box>
+      )}
     </Flex>
   );
 }
