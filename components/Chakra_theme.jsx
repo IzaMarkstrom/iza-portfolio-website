d9a31d6e357b72dab08727c979d5d26cd6aef4b8
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-21 10:42:06 +0100

    Added second background color alternative

--- components/Chakra_theme.jsx
+++ components/Chakra_theme.jsx
@@ -5,9 +5,12 @@ const theme = extendTheme({
     primary: "rgb(99, 63, 211)",
     blue: "rgb(60, 85, 169)",
     background: "rgb(4, 0, 19)",
+    background2:  "rgb(38, 0, 77)",
     gray: "rgb(190, 190, 190)",
     darkGray: "rgb(79, 91, 102)"
   },
 });
+
+// Alternative colors #7928CA, #FF0080
 
 export default theme;
