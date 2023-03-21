be9fe7cb05cd1d8b0ff8bae2442063701cfeba05
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-21 10:41:17 +0100

    Added gradients and put it in Flex

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,7 +5,8 @@ import { Box,
     Heading,
     CardBody,
     SimpleGrid,
-    CardHeader
+    CardHeader,
+    Flex
 } from '@chakra-ui/react'
 import React from 'react'
 import theme from './Chakra_theme'
@@ -13,11 +14,19 @@ import theme from './Chakra_theme'
 export const Skills = () => {
   return (
     <Box mt='200px'>
-        <Text fontSize='3xl' mb={4}>Skills</Text>
-            <Text mb={4}>I prefer the MERN stack, but these are the different programming languages I have been coding with</Text> 
+        <Flex alignItems='center' flexDir='column'>
+            <Text fontSize='3xl' mb={4}>Skills</Text>
+            <Text fontSize='3xl'>I prefer the MERN stack, but these are the different programming languages I have been coding with</Text> 
+        </Flex>
+            <Flex justifyContent='center' flexDir='row'>
             <Box>
                 <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(150px, 1fr))'>
-                <Card alignItems='center' bg={theme.colors.gray} boxShadow='0 0 10px rgba(0, 0, 0, 0.2), 0 0 20px rgba(0, 0, 0, 0.1), 0 0 30px #FFFFFF'>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>HTML</Heading>
                     </CardHeader>
@@ -25,7 +34,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/html.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>CSS</Heading>
                     </CardHeader>
@@ -33,7 +47,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/css.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>JavaScript</Heading>
                     </CardHeader>
@@ -41,7 +60,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/javascript.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>React</Heading>
                     </CardHeader>
@@ -49,7 +73,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/react.png' /> 
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>Vue</Heading>
                     </CardHeader>
@@ -57,7 +86,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/rsz_1vuejs_logo_2svg.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>Node</Heading>
                     </CardHeader>
@@ -65,7 +99,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/node.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>MongoDB</Heading>
                     </CardHeader>
@@ -73,7 +112,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/mongo.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>NextJs</Heading>
                     </CardHeader>
@@ -81,7 +125,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/nextjs.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>PHP</Heading>
                     </CardHeader>
@@ -89,7 +138,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/rsz_21php-logosvg.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>Laravel</Heading>
                     </CardHeader>
@@ -97,7 +151,12 @@ export const Skills = () => {
                         <Image src='../assets/skills/rsz_311969px-laravelsvg.png' />
                     </CardBody>
                 </Card>
-                <Card alignItems='center' bg={theme.colors.gray}>
+                <Card 
+                    alignItems='center'                 
+                    bgGradient={`linear(to-bl, ${theme.colors.background} 50%, ${theme.colors.primary})`}
+                    border={`1px solid ${theme.colors.darkGray}`}
+                    borderRadius='10px'
+                    color='white'>
                     <CardHeader>
                         <Heading size='md'>Bootstrap</Heading>
                     </CardHeader>
@@ -107,6 +166,7 @@ export const Skills = () => {
                 </Card>
             </SimpleGrid>
             </Box>
+            </Flex>
 
 
             {/* <Grid templateColumns='repeat(3, 1fr)' gap={2} mr='100px' ml='100px'> */}
