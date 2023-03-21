7b2870c03a7e5473dd73cf5192f741cf6a32fe6b
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-03-21 10:41:42 +0100

    Changed the scrolling and bg color

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,59 +3,60 @@ import { Box, Flex, Text, Button, Spacer } from "@chakra-ui/react";
 import theme from './Chakra_theme';
 
 function Navbar() {
-  const [showBorder, setShowBorder] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const isTop = window.scrollY < 100;
-      setShowBorder(!isTop);
+      setIsScrolled(window.scrollY > 0);
     };
+
     window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
     <Flex
+      as="nav"
       position="fixed"
       top={0}
-      left={0}
-      right={0}
       p={4}
-      align="center"
-      borderBottomWidth="1px"
-      borderBottomColor="black"
-      backgroundColor="rgba(37, 59, 71, 0.9)"
-      boxShadow={showBorder ? '0 8px 16px rgba(0, 0, 1, 1)' : 'none'}
+      w="100%"
+      bg={isScrolled ? 'rgba(37, 59, 71, 0.9)' : 'transparent'}
+      boxShadow={isScrolled ? 'sm' : 'none'}
+      transition="all 0.3s ease-in-out"
       zIndex={1}
     >
       <Box>
-        <Text color="white" fontWeight="bold">My Portfolio</Text>
+        <Text color="white" fontWeight="bold" p={2}>My Portfolio</Text>
       </Box>
       <Spacer />
       <Box>
         <Button
             variant='solid' 
-            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`} 
             color='white'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>HOME</Button>
         <Button
             variant='solid' 
-            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>ABOUT</Button>
         <Button
             variant='solid' 
-            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>PROJECTS</Button>
         <Button
             variant='solid' 
-            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>SKILLS</Button>
         <Button
             variant='solid' 
-            bgGradient={`linear(to-r, #7928CA, #FF0080)`} 
+            bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.blue})`}  
             color='white'
             _hover={{boxShadow: `0 0 20px 3px ${theme.colors.blue}`}} mr={4}>CONTACT</Button>
       </Box>
