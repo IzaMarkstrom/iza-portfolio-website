2acd7ea819db054846ccbac970b676715d2dec09
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-03 17:20:08 +0200

    Changed color scheme

--- components/Chakra_theme.jsx
+++ components/Chakra_theme.jsx
@@ -2,36 +2,13 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   colors: {
-    lightGreen: "#e2f0db",
-    primary: "#4c9d7f",
-    lightBlue: "#98bec1",
-    darkGreen: "#022d3e",
+
+    background: "#d7b7e4",
+    primary: "#123945",
+    lightPurple: "#d7b7e4",
+    darkGreen: "#13bb8b",
     background2: "#cfe2ec",
-
-
-
-    // background: "#d7b7e4",
-    // primary: "#952fc4",
-    // lightPurple: "#d7b7e4",
-    // yellow: "#fdbb2d",
-    // background2: "#cfe2ec",
-
-
-    // background: "#eaf6e8",
-    // primary: "#212e33",
-    // darkGray: "rgb(255, 158, 177)",
-    // blue: "#02d1b7",
-    // background2: "#cfe2ec",
-
-    // primary: "rgb(99, 63, 211)",
-    // blue: "rgb(60, 85, 169)",
-    // background: "rgb(4, 0, 19)",
-    // background2:  "rgb(38, 0, 77)",
-    // gray: "rgb(190, 190, 190)",
-    // darkGray: "rgb(79, 91, 102)"
   },
 });
-
-// Alternative colors #7928CA, #FF0080
 
 export default theme;
