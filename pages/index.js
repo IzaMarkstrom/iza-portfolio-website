a983a809d778a4ba7f1c0a6cc42f50237c037a5c
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-04 10:11:55 +0200

    Removed dangerouslySetInnerHTML

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
       <meta name="keywords" content="portfolio" charSet="UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <script dangerouslySetInnerHTML={{ __html: 'history.scrollRestoration = "manual";' }} />
     </Head>
 
     <main>
