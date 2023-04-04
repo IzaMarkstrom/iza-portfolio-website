fc20ec2a2edb5e841d402bccfbfe2862c9bcda54
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2023-04-04 17:35:01 +0200

    Removed unused code & fixed indetation and tabs

--- components/About.jsx
+++ components/About.jsx
@@ -24,15 +24,7 @@ function About() {
                 mb={10}
                 color='white' 
                 as='b'
-                id='about'
-                borderBottom="2px"
-                borderBottomColor={{
-                  base: "linear-gradient(90deg, #13bb8b, #123945)",
-                  md: "linear-gradient(90deg, #123945, #13bb8b)"
-                }}
-                css={{
-                  animation: 'line-appear 1s ease-out forwards'
-                }}>
+                id='about'>
                 About
             </Text>
         </Flex> 
@@ -46,74 +38,74 @@ function About() {
 
             {isMobile ? ( 
                 <>
-                    <Grid id='about_me' borderRadius='2rem' mb={7} h='300px' minW='300px' maxW='300px'>
-                        <Image src='../assets/me2.jpg' alt='me' id='about_me_image' borderRadius='2rem' />
-                    </Grid>
-                    
-                    <Grid gridTemplateColumns='repeat(auto-fit, minmax(250px, 1fr))'  gap='5%'>
+                    <Grid gridTemplateColumns='repeat(auto-fit, minmax(150px, 1fr))' justifyItems='center' gap='5%'>
 
-                <Box className='flip_card' h='200px' minW='200px' maxW='200px'>
-                    <Box className='flip_card_inner'>
-                        <Box className='flip_card_front' borderRadius='2rem'>
-                            <Text color='white' fontSize='xl' textAlign='center' mt={4}>Shuffle</Text>
+                        <Box className='flip_card' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}>
+                            <Box className='flip_card_inner'>
+                                <Box className='flip_card_front' borderRadius='2rem'>
+                                    <Text color='white' fontSize='xl' textAlign='center' mt={4}>Shuffle</Text>
+                                </Box>
+                                <Box className='flip_card_back'>
+                                    <Image src='../assets/rsz_shuffle.jpg' alt='me in a shuffle studio' borderRadius='2rem' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}/>
+                                </Box>
+                            </Box>
                         </Box>
-                        <Box className='flip_card_back'>
-                            <Image src='../assets/rsz_shuffle.jpg' alt='me in a shuffle studio' borderRadius='2rem' h='200px' minW='200px' maxW='200px'/>
-                        </Box>
-                    </Box>
-                </Box>
 
-                <Box className='flip_card' h='200px' minW='200px' maxW='200px'>
-                    <Box className='flip_card_inner'>
-                        <Box className='flip_card_front' borderRadius='2rem'>
-                            <Text color='white' fontSize='xl' textAlign='center' mt={4}>Debugging buddy</Text>
+                        <Box className='flip_card' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}>
+                            <Box className='flip_card_inner'>
+                                <Box className='flip_card_front' borderRadius='2rem'>
+                                    <Text color='white' fontSize='xl' textAlign='center' mt={4}>Debugging buddy</Text>
+                                </Box>
+                                <Box className='flip_card_back'>
+                                    <Image src='../assets/rsz_11okie.jpg' alt='racoon stuffed animal' borderRadius='2rem' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}/>
+                                </Box>
+                            </Box>
                         </Box>
-                        <Box className='flip_card_back'>
-                            <Image src='../assets/rsz_11okie.jpg' alt='racoon stuffed animal' borderRadius='2rem' h='200px' minW='200px' maxW='200px'/>
-                        </Box>
-                    </Box>
-                </Box>
 
 
-                <Box className='flip_card' h='200px' minW='200px' maxW='200px'>
-                    <Box className='flip_card_inner'>
-                        <Box className='flip_card_front' borderRadius='2rem'>
-                            <Text color='white' fontSize='xl' textAlign='center' mt={4}>Pirate</Text>
+                        <Box className='flip_card' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}>
+                            <Box className='flip_card_inner'>
+                                <Box className='flip_card_front' borderRadius='2rem'>
+                                    <Text color='white' fontSize='xl' textAlign='center' mt={4}>Pirate</Text>
+                                </Box>
+                                <Box className='flip_card_back'>
+                                    <Image src='../assets/rsz_pirate.jpg' alt='me in pirate outfit looking out to sea' borderRadius='2rem' h={isMobile ? '130px' : '200px'} minW={isMobile ? '130px' : '200px'} maxW={isMobile ? '130px' : '200px'}/>
+                                </Box>
+                            </Box>
                         </Box>
-                        <Box className='flip_card_back'>
-                            <Image src='../assets/rsz_pirate.jpg' alt='me in pirate outfit looking out to sea' borderRadius='2rem' h='200px' minW='200px' maxW='200px'/>
-                        </Box>
-                    </Box>
-                </Box>
 
                     </Grid>
-
-                <GridItem>
-                    <Text fontSize='m' color='white'>
-                        I&apos;m a full stack web developer with a passion for learning new technologies. 
-                        I&apos;m currently working on a project that uses React, TypeScript, Node.js, 
-                        Express, and MongoDB. Instead of a rubber duck helping me to debug my code, using the method rubber-duck-debugging, 
-                        I have a different animal as you can see in one of the pictures above.
-                        I come from a background of many years in the travel industry. 
-                        The pandemic let me explore my interest for programming and set me on a path of pursuing 
-                        my dream of becomming a web developer. Apart from coding, I love to dance shuffle and going to both small and big concerts.
-                        I also sew my own pirate clothes for the Medieval week on Gotland. 
-                    </Text>
-                </GridItem>
-                <GridItem>
-                    <Button 
-                        variant='solid' 
-                        bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.darkGreen})`} 
-                        color='white'
-                        _hover={{p: '15px'}}
-                        p='10px'
-                        borderRadius='20px'
-                        >
-                                <NextLink href='#contact'>
-                                    Let&apos;s talk!
-                                </NextLink>
-                        </Button>
-                </GridItem>
+                    <Grid gap='5%' justifyItems='center' pl={10} pr={10}>
+                            <Text fontSize='m' color='white'>
+                                I&apos;m a full stack web developer with a passion for learning new technologies. 
+                                Instead of a rubber duck helping me to debug my code, using the method rubber-duck-debugging, 
+                                I have a different animal as you can see in one of the pictures above.
+                            </Text>
+                            <Text>
+                                I come from a background of many years in the travel industry, where I worked as a travel agent. I'm still always looking forward towards my next trip!
+                                The pandemic let me explore my interest for programming and set me on a path of pursuing 
+                                my dream of becomming a web developer. Apart from coding, I love to dance shuffle and going to both small and big concerts.
+                                I also sew my own pirate clothes for the Medieval week on Gotland. 
+                            </Text>
+                        <Grid id='about_me' orderRadius='2rem' mb={7} h={isMobile ? '180px' : '300px'} minW={isMobile ? '180px' : '300px'} maxW={isMobile ? '180px' : '300px'}>
+                                <Image src='../assets/me2.jpg' alt='me' id='about_me_image' borderRadius='2rem' />
+                        </Grid>
+                        
+                        <GridItem>
+                            <Button 
+                                variant='solid' 
+                                bgGradient={`linear(to-r, ${theme.colors.primary}, ${theme.colors.darkGreen})`} 
+                                color='white'
+                                _hover={{p: '15px'}}
+                                p='10px'
+                                borderRadius='20px'
+                                >
+                                        <NextLink href='#contact'>
+                                            Let&apos;s talk!
+                                        </NextLink>
+                                </Button>
+                        </GridItem>
+                    </Grid>
                     </>
             ) : (  
                 <>         
@@ -157,16 +149,17 @@ function About() {
                         </Box>
 
                         <GridItem colSpan={3}>
-                            <Text fontSize='l' color='white'>
+                        <Text fontSize='m' color='white'>
                             I&apos;m a full stack web developer with a passion for learning new technologies. 
-                            I&apos;m currently working on a project that uses React, TypeScript, Node.js, 
-                            Express, and MongoDB.  Instead of a rubber duck helping me to debug my code, using the method rubber-duck-debugging, 
+                            Instead of a rubber duck helping me to debug my code, using the method rubber-duck-debugging, 
                             I have a different animal as you can see in one of the pictures above.
-                            I come from a background of many years in the travel industry. 
+                        </Text> <br />
+                        <Text>
+                            I come from a background of many years in the travel industry, where I worked as a travel agent. I&apos;m still always looking forward towards my next trip!
                             The pandemic let me explore my interest for programming and set me on a path of pursuing 
-                            my dream of becomming a web developer. Apart from coding, I love to dance shuffle and going to both small and big concerts.
+                            my dream of becomming a web developer instead. Apart from coding, I love to dance shuffle and going to both small and big concerts.
                             I also sew my own pirate clothes for the Medieval week on Gotland. 
-                            </Text>
+                        </Text>
                         </GridItem>
                         <GridItem>
                             <Button 
